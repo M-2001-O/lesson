7e089862918cdc751bb7e23e6f02b7e60f546195
Maina Matsubara <c20029@std.it-college.ac.jp>
2021-06-02 17:35:02 +0900

    finished challenge p69

---
+++ chap4/c3.py
@@ -0,0 +1,6 @@
+dif num(a, b, c, d = 3, e = 4):
+    return a + b + c + d + e
+
+print(num(1,2,3))
+print(num(1,2,3,4))
+print(num(1,2,3,4,5))
---
+++ chap4/c4.py
@@ -0,0 +1,8 @@
+dif f1(x):
+    return x / 2
+
+result = f1(6)
+dif f2(result):
+    return result * 4
+
+print(result)
